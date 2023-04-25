925e2ad0717530923fd46f743459e6dbb348e01b
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-04-25 23:35:02 +0530

    Add comments

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -5,20 +5,29 @@ export default function Dropdown({ items, onItemSelected, selectedItemIndex }) {
     const [showMenu, setShowMenu] = useState(false);
     useEffect(() => addDocumentClickListener(), []);
 
+    // document click listener
     function addDocumentClickListener() {
         document.addEventListener("click", handleDocumentClick);
         return () => document.removeEventListener("click", handleDocumentClick);
     }
 
+    // document click listener is used to close the menu
+    // because the objective is to close the menu whenever
+    // a click occurs outside of the component
     const handleDocumentClick = () => setShowMenu(false);
 
+    // on mouse hover, menu will be shown
     const handleMouseEnter = () => setShowMenu(true);
 
+    // when menu is clicked, menu will be closed
+    // also stopping the event propagation so that
+    // it doesn't reach document
     function handleMenuClick(event) {
         event.stopPropagation();
         setShowMenu(false);
     }
 
+    // raising onItemSelected event
     function handleMenuItemClick(index) {
         onItemSelected && onItemSelected(index);
     }
